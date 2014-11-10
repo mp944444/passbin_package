0404bc3209a0bb28d0bc1c4701b094fcd8fa8bb4
Patrick Oberdorf <patrick@oberdorf.net>
2014-11-11 00:39:57 +0100

    removed uneeded stuff from base controller

--- Classes/Passbin/Base/Controller/BaseController.php
+++ Classes/Passbin/Base/Controller/BaseController.php
@@ -10,13 +10,5 @@ use TYPO3\Flow\Annotations as Flow;
 
 class BaseController extends \TYPO3\Flow\Mvc\Controller\ActionController {
 
-	/**
-	 * @return void
-	 */
-	public function indexAction() {
-		$this->view->assign('foos', array(
-			'bar', 'baz'
-		));
-	}
 
 }
