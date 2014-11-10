c97b4e73de8045723bfe5b43dbfa96331894f94b
Patrick Oberdorf <patrick@oberdorf.net>
2014-11-10 22:41:42 +0100

    unit test for pass

---
+++ Tests/Unit/Domain/Model/PassTest.php
@@ -0,0 +1,24 @@
+<?php
+namespace Passbin\Base\Tests\Unit\Domain\Model;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "Passbin.Base".          *
+ *                                                                        *
+ *                                                                        */
+
+/**
+ * Testcase for Pass
+ */
+class PassTest extends \TYPO3\Flow\Tests\UnitTestCase {
+
+	/**
+	 * @test
+	 */
+	public function makeSureThatSomethingHolds() {
+		$this->markTestIncomplete('Automatically generated test case; you need to adjust this!');
+
+		$expected = 'Foo';
+		$actual = 'Foo'; // This should be the result of some function call
+		$this->assertSame($expected, $actual);
+	}
+}
