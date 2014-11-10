0d3c2cfd1929a75f0e814284c9ff8e32101bea96
Patrick Oberdorf <patrick@oberdorf.net>
2014-11-10 21:29:07 +0100

    Added Base Controller

---
+++ Classes/Passbin/Base/Controller/BaseController.php
@@ -0,0 +1,22 @@
+<?php
+namespace Passbin\Base\Controller;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "Passbin.Base".          *
+ *                                                                        *
+ *                                                                        */
+
+use TYPO3\Flow\Annotations as Flow;
+
+class BaseController extends \TYPO3\Flow\Mvc\Controller\ActionController {
+
+	/**
+	 * @return void
+	 */
+	public function indexAction() {
+		$this->view->assign('foos', array(
+			'bar', 'baz'
+		));
+	}
+
+}
