85e8b804b72636f9cba22e998c71f48a8a2d1f31
Patrick Oberdorf <patrick@oberdorf.net>
2014-11-10 22:40:29 +0100

    added createPass controller

---
+++ Classes/Passbin/Base/Controller/CreatePassController.php
@@ -0,0 +1,31 @@
+<?php
+namespace Passbin\Base\Controller;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "Passbin.Base".          *
+ *                                                                        *
+ *                                                                        */
+
+use TYPO3\Flow\Annotations as Flow;
+
+class CreatePassController extends \TYPO3\Flow\Mvc\Controller\ActionController {
+
+	/**
+	 * @return void
+	 */
+	public function indexAction() {
+		$this->view->assign('foos', array(
+			'bar', 'baz'
+		));
+	}
+
+    /**
+     * @return void
+     */
+    public function newAction() {
+        $this->view->assign('foos', array(
+            'bar', 'baz'
+        ));
+    }
+
+}
