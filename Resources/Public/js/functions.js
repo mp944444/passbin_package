1ec4b12e9a8ba6b03a6a519a9c31151e291db305
Patrick Oberdorf <patrick@oberdorf.net>
2014-11-11 00:59:11 +0100

    added jquery and email will be inactive at first visit

--- Resources/Public/js/functions.js
+++ Resources/Public/js/functions.js
@@ -1,3 +1,11 @@
-/**
- * Created by patrick on 11.11.14.
- */
+$(document).ready(function() {
+
+    $('#sendEmail').change(function() {
+        if($(this).is(":checked")) {
+            $('#email').parent().parent().slideDown();
+        } else {
+            $('#email').parent().parent().slideUp();
+            $('#email').val('');
+        }
+    });
+});
