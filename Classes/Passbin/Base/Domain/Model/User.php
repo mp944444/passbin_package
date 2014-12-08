47b11129b57ed9814ff5bdcd87bcfc583c3600ae
Marcel Petersen <m.petersen@raphael-gmbh.de>
2014-12-08 14:31:26 +0100

    [TASK] Created Model User

---
+++ Classes/Passbin/Base/Domain/Model/User.php
@@ -0,0 +1,18 @@
+<?php
+namespace Passbin\Base\Domain\Model;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "Passbin.Base".          *
+ *                                                                        *
+ *                                                                        */
+
+use TYPO3\Flow\Annotations as Flow;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @Flow\Entity
+ */
+class User {
+
+
+}
