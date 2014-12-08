290bbc92424761c3c18c2a3ebc05f2a740c5d948
Marcel Petersen <m.petersen@raphael-gmbh.de>
2014-12-08 14:37:01 +0100

    [TASK] Created UserRepository

---
+++ Classes/Passbin/Base/Domain/Repository/UserRepository.php
@@ -0,0 +1,19 @@
+<?php
+namespace Passbin\Base\Domain\Repository;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "Passbin.Base".          *
+ *                                                                        *
+ *                                                                        */
+
+use TYPO3\Flow\Annotations as Flow;
+use TYPO3\Flow\Persistence\Repository;
+
+/**
+ * @Flow\Scope("singleton")
+ */
+class UserRepository extends Repository {
+
+	// add customized methods here
+
+}
