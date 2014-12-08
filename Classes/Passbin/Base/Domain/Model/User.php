f45545cfd7bdbba79f89cb81a2cb67acfe31367f
Marcel Petersen <m.petersen@raphael-gmbh.de>
2014-12-08 14:52:19 +0100

    [TASK] User Model

--- Classes/Passbin/Base/Domain/Model/User.php
+++ Classes/Passbin/Base/Domain/Model/User.php
@@ -13,6 +13,67 @@ use Doctrine\ORM\Mapping as ORM;
  * @Flow\Entity
  */
 class User {
+	/**
+	 * @var \TYPO3\Flow\Security\Account
+	 * @ORM\OneToOne
+	 */
+	protected $account;
 
+	/**
+	 * @var string
+	 */
+	protected $firstname;
 
+	/**
+	 * @var string
+	 */
+	protected $lastname;
+
+	/**
+	 * @return \TYPO3\Flow\Security\Account
+	 */
+	public function getAccount()
+	{
+		return $this->account;
+	}
+
+	/**
+	 * @param \TYPO3\Flow\Security\Account $account
+	 */
+	public function setAccount($account)
+	{
+		$this->account = $account;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getFirstname()
+	{
+		return $this->firstname;
+	}
+
+	/**
+	 * @param string $firstname
+	 */
+	public function setFirstname($firstname)
+	{
+		$this->firstname = $firstname;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getLastname()
+	{
+		return $this->lastname;
+	}
+
+	/**
+	 * @param string $lastname
+	 */
+	public function setLastname($lastname)
+	{
+		$this->lastname = $lastname;
+	}
 }
