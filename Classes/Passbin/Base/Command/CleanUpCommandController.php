6125c154d5cebd78ee9e4e172733e33bd6dad4eb
Marcel Petersen <m.petersen@raphael-gmbh.de>
2014-12-22 16:28:02 +0100

    [TASK] Smaller if for CommandController

--- Classes/Passbin/Base/Command/CleanUpCommandController.php
+++ Classes/Passbin/Base/Command/CleanUpCommandController.php
@@ -32,15 +32,15 @@ class CleanUpCommandController extends \TYPO3\Flow\Cli\CommandController {
 	 * Delete old notes
 	 *
 	 * @param string $date all entries which are created before this date will be deleted
-	 * @param bool $mustExpired must entry be expired? TRUE/FALSE
-	 * @param bool $userNotes delete notes from registered users? TRUE/FALSE
-	 * @param bool $nonUserNotes delete notes from unregistered users? TRUE/FALSE
+	 * @param bool $mustExpired must entry be expired? [TRUE/FALSE]
+	 * @param bool $userNotes delete notes from registered users? [TRUE/FALSE]
+	 * @param bool $nonUserNotes delete notes from unregistered users? [TRUE/FALSE]
 	 */
 	public function deleteOldNotesCommand($date, $mustExpired = TRUE, $userNotes = FALSE, $nonUserNotes = TRUE) {
 		$count = 0;
 		if($userNotes == FALSE && $nonUserNotes == FALSE) {
 			$entries = NULL;
-			$this->outputLine("userNotes and nonUserNotes can be not FALSE");
+			$this->outputLine("userNotes and nonUserNotes can not both be FALSE");
 		} else if($userNotes == TRUE && $nonUserNotes == FALSE) {
 			$entries = $this->passRepository->findAllByUser(TRUE);
 		} else if($userNotes == FALSE && $nonUserNotes == TRUE) {
@@ -52,13 +52,10 @@ class CleanUpCommandController extends \TYPO3\Flow\Cli\CommandController {
 		if($entries != NULL) {
 			foreach($entries as $entry) {
 				/** @var Pass $entry */
-				if($mustExpired && $entry->getExpiration() < new \DateTime('now') && $entry->getCreationDate() < new \DateTime($date)) {
+				if($mustExpired && $entry->getExpiration() < new \DateTime('now') && $entry->getCreationDate() < new \DateTime($date) || $mustExpired && $entry->getCallable() == 0) {
 					$this->passRepository->remove($entry);
 					$count++;
 				} else if($mustExpired == FALSE && $entry->getCreationDate() < new \DateTime($date)) {
-					$this->passRepository->remove($entry);
-					$count++;
-				} else if($mustExpired && $entry->getCallable() == 0) {
 					$this->passRepository->remove($entry);
 					$count++;
 				}
@@ -71,7 +68,7 @@ class CleanUpCommandController extends \TYPO3\Flow\Cli\CommandController {
 	 * Delete inactive Users
 	 *
 	 * @param string $lastLogin all users who have not logged in after this date will be deleted
-	 * @param boolean $activatedAccounts Acitvated accounts will be deleted too
+	 * @param boolean $activatedAccounts Activated accounts will be deleted too [TRUE/FALSE]
 	 */
 	public function deleteInactiveUserCommand($lastLogin, $activatedAccounts = FALSE) {
 		$count = 0;
